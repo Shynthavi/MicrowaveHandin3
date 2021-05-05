aa5366d54a379f25b66d109db9491bdb25003aeb
Henry Pham <HenryPham@live.dk>
2021-05-05 08:39:05 +0200

    gitignore updated and integrationstep2 added

--- MicrowaveOvenCore/Microwave.Test.Integrationtest/IntegrationStep2.cs
+++ MicrowaveOvenCore/Microwave.Test.Integrationtest/IntegrationStep2.cs
@@ -10,11 +10,72 @@ namespace Microwave.Test.Integrationtest
     [TestFixture]
     public class IntegrationStep2
     {
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+        private IDoor _door;
+        private ICookController _cookController;
+        private IDisplay _display;
+        private ILight _light;
+        private IUserInterface _userInterface;
+        private IOutput _output;
 
         [SetUp]
         public void Setup()
         {
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _door = new Door();
+            _cookController = Substitute.For<ICookController>();
+            _display = Substitute.For<IDisplay>();
+            _output = Substitute.For<IOutput>();
+            _light = new Light(_output);
 
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light,
+                _cookController);
         }
+
+
+        #region Light
+
+        [Test]
+        public void Door_Opened_LightOn_Output()
+        {
+            //Act
+            _door.Open();
+
+            //Assert
+            _output.Received(1).OutputLine("Light is turned on");
+        }
+
+        [Test]
+        public void Door_Closed_LightOff_Output()
+        {
+            //Act
+            _door.Open();
+            _door.Close();
+
+            //Assert
+            _output.Received(1).OutputLine("Light is turned off");
+        }
+        #endregion
+
+        #region Display
+
+        //ShowPower(), ShowTime(), Clear()
+        [Test]
+        public void PowerButton_PressOnce_ShowPower()
+        {
+            //Act
+            _powerButton.Press();
+
+            //Assert
+            _output.Received(1).OutputLine($"Display shows: 50 W");
+        }
+
+
+        #endregion
+
     }
 }
