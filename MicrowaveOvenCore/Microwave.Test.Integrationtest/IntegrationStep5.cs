17d3fff10d36eb25a76ea3377c0ac0d2bcf7645b
Henry Pham <HenryPham@live.dk>
2021-05-05 12:00:31 +0200

    Integration step 5 added

---
+++ MicrowaveOvenCore/Microwave.Test.Integrationtest/IntegrationStep5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integrationtest
+{
+    [TestFixture]
+    public class IntegrationStep5
+    {
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+        private IDoor _door;
+        private CookController _cookController;
+        private IDisplay _display;
+        private ILight _light;
+        private IUserInterface _userInterface;
+        private IOutput _output;
+        private IPowerTube _powerTube;
+        private ITimer _timer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _door = new Door();
+            _output = Substitute.For<IOutput>();
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _powerTube = Substitute.For<IPowerTube>();
+            _timer = Substitute.For<ITimer>();
+
+            _cookController = new CookController(_timer, _display, _powerTube);
+
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light,
+                _cookController);
+
+            //Property dependency injection
+            _cookController.UI = _userInterface;
+        }
+    }
+}
